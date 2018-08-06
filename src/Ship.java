eae36c0ad905ad639175304738277d2e98e687e0
original commit <>
2018-08-05 19:19:28 -0600

    Finish the fleet class. Made changes to the ship class to help complete the fleet class.

--- src/Ship.java
+++ src/Ship.java
@@ -5,27 +5,45 @@ public class Ship {
     private String shipName;
 
 
-    public enum shipStatus {Hit, Miss, Sunk}
+    public enum shipStatus {Active, Sunk}
+    public enum shipNames{BATTLESHIP, CRUISER, SUBMARINE, CARRIER, DESTROYER}
 
-
-    public Ship(String shipName, int hitCount) {
-        this.shipName = shipName;
-        this.hitCount = hitCount;
+    public Ship(int shipID, String shipName, int hitCount, String status) {
+        setShipName(shipName);
+        setHitCount(hitCount);
+        setShipID(shipID);
+        setShipStatus(status);
     }
 
-
-
+    //get methods
     public String getShipName() {
-        return shipName;
+        return this.shipName;
+    }
+    public int getShipID() {
+        return this.shipID;
     }
 
-    public void setShipID() {
+    public int getHitCount(){
+        return this.hitCount;
+    }
+    public String getShipStatus(){
+        return this.status;
+    }
+
+    //set methods
+    public void setShipID(int shipID) {
         this.shipID = shipID;
     }
 
-    public int getShipByID() {
-        return shipID;
+    public void setShipName(String shipName){
+        this.shipName = shipName;
     }
 
+    public void setHitCount(int hitCount){
+        this.hitCount = hitCount;
+    }
+    public void setShipStatus(String status){
+        this.status = status;
+    }
 
 }
