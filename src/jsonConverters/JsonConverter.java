3341772f070c85cb4ae1d930c70ba7968ffced7b
New2017Bie <boyu@mail.weber.edu>
2018-08-06 03:50:58 -0600

    Import the gson lib, created a JsonString Converter, a MessageWrapper class to meet the format in API Document.

---
+++ src/jsonConverters/JsonConverter.java
@@ -0,0 +1,29 @@
+package jsonConverters;
+
+import Message.Message;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+
+public class JsonConverter {
+
+    private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public static String writeJson(MessageWrapper mw) {
+        String content = gson.toJson(mw);
+
+        return content;
+    }
+
+    public static Message readJson(String jsonFile) {
+        MessageWrapper mw;
+
+        try {
+            mw = gson.fromJson(jsonFile, MessageWrapper.class);
+        } catch (JsonSyntaxException e) {
+            throw new JsonSyntaxException("Json data corrupted.");
+        }
+
+        return mw.getMessage();
+    }
+}
